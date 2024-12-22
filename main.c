f581e8324737746c53a40857f609f4a8f2580efd
Kevin Odeyemi <85255346+Kevinart-maker@users.noreply.github.com>
2024-12-22 20:25:22 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   /*   int r ;
+  int cube;
+
+  printf("Please enter a number: ");
+  scanf("%d",&r);
+
+
+  while(r<10){
+        cube = r*r*r;
+     printf("Number is %d, and the cube is: %d\n",r,cube);
+     r++;
+  }
+  printf("\nCompleted!\n");
+
+  int c=0;
+  while (c<10){
+    int t=0;
+    while(t<10){
+        printf("%d",t);
+  t++;
+    }
+    printf("\n");
+    c++;
+  }
+
+
+    int count1,count2;
+    for(count1=1,count2=0;count1<=100;count1++)
+    {
+        printf("Enter %d count2: ",count1);
+        scanf("%d",&count2);
+        if(count1==4) break;
+    }
+
+    int
+    for(;;)
+    {
+
+    }
+
+
+   int num;
+   for(num=1; num<=100; num++)
+   {
+
+       if(num % 9==0)
+        continue;
+       printf("%d\t",num);
+   }*/
+
+
+
+
+
+
+
+    return 0;
+}
